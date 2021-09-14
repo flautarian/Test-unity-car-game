d776552603ad3d16e9d3aeb8b02c9c387c80d15a
Flautarian <flautarian@gmail.com>
2021-09-15 00:24:23 +0200

    [WIP]Adapting countdown

--- Assets/Scripts/CountStart.cs
+++ Assets/Scripts/CountStart.cs
@@ -7,38 +7,18 @@ public class CountStart : MonoBehaviour
 {
     public Sprite[] spriteArray;
     public AudioClip[] audioClips;
+    private Animator animator;
     private AudioSource audioSource;
-    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        StartCoroutine(backCountTime());
+        animator = GetComponent<Animator>();
     }
 
-    private IEnumerator backCountTime()
+    public void StartGame()
     {
-
-        audioSource.Play();
-
-        yield return new WaitForSeconds(1);
-        spriteRenderer.sprite = spriteArray[1];
-        audioSource.Play();
-
-        yield return new WaitForSeconds(1);
-        spriteRenderer.sprite = spriteArray[0];
-        audioSource.Play();
-
-        yield return new WaitForSeconds(1);
-        spriteRenderer.sprite = spriteArray[3];
-        audioSource.clip = audioClips[1];
-        audioSource.Play();
-
         GameObject timer = GameObject.FindGameObjectWithTag(Constants.GO_TAG_GUI);
         if (timer != null) timer.GetComponent<GUIController>().startGame();
-        yield return new WaitForSeconds(2);
-        spriteRenderer.enabled = false;
-        Destroy(this.gameObject);
     }
 }
